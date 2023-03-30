f3c272269021643773541f0f11d06c6857fba483
Renzo1611 <renzo.16110@gmail.com>
2023-03-29 19:04:52 -0500

    Agregando las validaciones del formulario

--- JS/formularios.js
+++ JS/formularios.js
@@ -1,19 +1,61 @@
-formulario.addEventListener('submit', async (event) => {
-    event.preventDefault();
+const formulario = document.querySelector('.formulario-cuerpo');
+const nombreInput = document.getElementById('nombre');
+const emailInput = document.getElementById('email');
+const asuntoInput = document.getElementById('asunto');
+const mensajeInput = document.getElementById('mensaje');
+const botonEnviar = document.querySelector('.botonEnviar');
+
+formulario.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (nombreInput.value.trim() === '' || nombreInput.value.length > 50) {
+    alert('El nombre debe tener entre 1 y 50 caracteres');
+    nombreInput.focus();
+    return;
+  }
+
   
-    const formData = new FormData(formulario);
-    const response = await fetch('/enviar-mensaje', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(Object.fromEntries(formData))
-    });
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailInput.value.trim() === '' || !emailRegex.test(emailInput.value)) {
+    alert('Ingrese un correo electrónico válido');
+    emailInput.focus();
+    return;
+  }
+
   
-    if (response.ok) {
-      alert('¡Mensaje enviado con éxito!');
-      formulario.reset();
-    } else {
-      alert('Error al enviar el mensaje. Intente de nuevo más tarde.');
-    }
-  });
+  if (asuntoInput.value.trim() === '' || asuntoInput.value.length > 50) {
+    alert('El asunto debe tener entre 1 y 50 caracteres');
+    asuntoInput.focus();
+    return;
+  }
+
+  
+  if (mensajeInput.value.trim() === '' || mensajeInput.value.length > 300) {
+    alert('El mensaje debe tener entre 1 y 300 caracteres');
+    mensajeInput.focus();
+    return;
+  }
+
+  
+  alert('Mensaje enviado correctamente');
+  formulario.submit();
+});
+
+
+nombreInput.addEventListener('input', toggleBotonEnviar);
+emailInput.addEventListener('input', toggleBotonEnviar);
+asuntoInput.addEventListener('input', toggleBotonEnviar);
+mensajeInput.addEventListener('input', toggleBotonEnviar);
+
+function toggleBotonEnviar() {
+  if (
+    nombreInput.value.trim() !== '' &&
+    emailInput.value.trim() !== '' &&
+    asuntoInput.value.trim() !== '' &&
+    mensajeInput.value.trim() !== ''
+  ) {
+    botonEnviar.removeAttribute('disabled');
+  } else {
+    botonEnviar.setAttribute('disabled', 'disabled');
+  }
+}
